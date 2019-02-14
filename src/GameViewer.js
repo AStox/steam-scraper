b3b77e056e7f494329d848e306d5bb58af883c61
Adam Stockermans <adamstox@gmail.com>
2019-02-14 12:14:51 -0500

    Added dynamic y-axis switching on graph. Variable still hard coded in but it works

--- src/GameViewer.js
+++ src/GameViewer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import {Query} from 'react-apollo';
-import {Table} from 'reactstrap';
+import {Bar} from 'react-chartjs-2';
 
 export const GET_GAMES = gql`
   query GetGames {
@@ -9,33 +9,36 @@ export const GET_GAMES = gql`
       id
       name
       full_price
+      review_count
     }
   }
 `;
+
+const y = 'review_count';
 
 export default () => (
   <Query query={GET_GAMES}>
     {({data, loading, error}) => {
       if (loading) return <p>LOADING</p>;
       if (error) return <p>{error.toString()}</p>;
-      return (
-        <Table>
-          <thead>
-            <tr>
-              <th>Author</th>
-              <th>Body</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.games.map(game => (
-              <tr key={game.id}>
-                <td>{game.name}</td>
-                <td>{game.full_price}</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      );
+      data.games.sort((a, b) => b[y] - a[y]);
+      let gameLabels = data.games.map(game => game.name);
+      let gameData = data.games.map(game => game[y]);
+      {
+        console.log(gameData);
+      }
+      let chartData = {
+        labels: gameLabels,
+        datasets: [
+          {
+            label: 'games by price',
+            data: gameData,
+            borderWidth: 0,
+          },
+        ],
+      };
+      let chartOptions = {};
+      return <Bar data={chartData} width={100} height={50} />;
     }}
   </Query>
 );
